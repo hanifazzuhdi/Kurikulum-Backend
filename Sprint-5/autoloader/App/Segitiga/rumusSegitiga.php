2cb622370313f39de209303d408ba056c5d00bbb
hanifazzuhdi <hnfhanif52@gmail.com>
2020-10-07 17:02:17 +0700

    file tugas

--- Sprint-5/autoloader/App/Segitiga/rumusSegitiga.php
+++ Sprint-5/autoloader/App/Segitiga/rumusSegitiga.php
@@ -2,11 +2,10 @@
 
 class rumusSegitiga
 {
-
     public function __construct($a = 0, $t = 0)
     {
         $luas = 0.5 * $a * $t;
 
-        echo "Luas Lingkaran = " . $luas;
+        echo "Luas Segitiga = " . $luas;
     }
 }
