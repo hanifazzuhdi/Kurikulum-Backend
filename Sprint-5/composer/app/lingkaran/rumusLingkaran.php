5e21a20da150fe0f8ce34c5a78f893fa495f9e65
hanifazzuhdi <hnfhanif52@gmail.com>
2020-10-07 16:57:08 +0700

    Tambah file Tugas Composer

---
+++ Sprint-5/composer/app/lingkaran/rumusLingkaran.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lingkaran;
+
+class rumusLingkaran
+{
+    public function __construct($r = 0)
+    {
+        $luas = pi() * $r * $r;
+        $keliling = 2 * pi() * $r;
+
+        echo "Luas Lingkaran = " . $luas;
+        echo "<br>";
+        echo "Keliling Lingkaran = " . $keliling;
+    }
+}
---
+++ Sprint-5/composer/app/segitiga/rumusSegitiga.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace segitiga;
+
+class rumusSegitiga
+{
+    public function __construct($a = 0, $t = 0)
+    {
+        $luas = 0.5 * $a * $t;
+
+        echo "Luas Lingkaran = " . $luas;
+    }
+}
