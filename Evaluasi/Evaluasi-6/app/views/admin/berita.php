82aeed326a50307c9523d6b7c8b92eefdc1f5283
hanifazzuhdi <hnfhanif52@gmail.com>
2020-10-19 08:45:33 +0700

    tambahkan link di button lihat berita.php

--- Evaluasi/Evaluasi-6/app/views/admin/berita.php
+++ Evaluasi/Evaluasi-6/app/views/admin/berita.php
@@ -18,7 +18,7 @@
                         <h5><?= $data["judul_berita"] ?></h5>
                         <p>Kategori : <?= $data["kategori"] ?></p>
                         <div class="aksi">
-                            <a href="Lihat">Lihat</a>
+                            <a href="<?= BASEURL ?>/home/berita/<?= $data["id_berita"] ?>">Lihat</a>
                             <a href="<?= BASEURL ?>/admin/getUbah/<?= $data["id_berita"] ?>">Ubah</a>
                             <a href="<?= BASEURL ?>/admin/delete/<?= $data["id_berita"] ?>" class="badge badge-danger float-right ml-2" onclick="return confirm('Yakin Hapus ?')">Hapus</a>
                         </div>
